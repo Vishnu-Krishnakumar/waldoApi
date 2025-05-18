07208083d31a357a4339c70b9f4723bb77feaae4
Vishnu-Krishnakumar <vkrish3@wgu.edu>
2025-05-18 10:08:49 +0530

    added jwt startTimer for frontend

--- controller/timerController.js
+++ controller/timerController.js
@@ -1,6 +1,24 @@
+const jwt = require("jsonwebtoken");
 
 async function dateTime(req,res){
-  res.json(Date.now());
+
+  try {
+    jwt.sign({ start: Date.now() }, process.env.SECRET,{ expiresIn: '1h' }, (err, token) => {
+      res.cookie("auth_jwt", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "None",
+        maxAge: 60 * 60 * 1000,
+      });
+      res.json({
+        token,
+      });
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+  // res.json(Date.now());
 }
 
 module.exports ={
