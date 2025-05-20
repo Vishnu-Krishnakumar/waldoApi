540e83426c0bb355e7ea2f4bcf6681d59eb066f8
Vishnu-Krishnakumar <vkrish3@wgu.edu>
2025-05-20 18:00:32 +0530

    new cors

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
     cors({
-        origin:["http://localhost:5173","http://127.0.0.1:5173","https://waldoapi-r65s.onrender.com"],
+        origin:["http://localhost:5173","http://127.0.0.1:5173","https://waldoapi-r65s.onrender.com","https://waldo-app.onrender.com"],
         credentials: true,
     })
 )
